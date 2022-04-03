8fbc6a7b5f8c4fcf0bcd18c196ba84ece22787cd
Joseph Cardwell <joseph.cardwell@jbcwebservices.com>
2022-04-03 12:26:06 -0500

    join path for .env

--- dev9/main.js
+++ dev9/main.js
@@ -1,5 +1,5 @@
 const path = require('path')
-console.log(require('dotenv').config({ path: path.resolve(__dirname, '../.env') }))
+console.log(require('dotenv').config({ path: path.join(__dirname,'.env') }))
 
 const TeleBot = require('telebot');
 const ethers = require("ethers");
