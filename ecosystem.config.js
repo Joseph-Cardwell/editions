ca957ae5bf0395d85aa11c13dd10e4e7abe2a7cf
Joseph Cardwell <joseph.cardwell@jbcwebservices.com>
2022-04-03 18:20:34 -0500

    move config require to node command

---
+++ ecosystem.config.js
@@ -0,0 +1,106 @@
+module.exports = {
+    apps : [
+        {
+            name : "dev9",
+            script : "main.js",
+            cwd: '/home/sokuswap/editions/dev9',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/dev9/.env dotenv_config_debug=true'
+        },
+        {
+            name: "dual",
+            script: "main.js",
+            cwd: '/home/sokuswap/editions/dual',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/dual/.env dotenv_config_debug=true'
+        },
+        {
+            name:"centcex",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/centcex',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/centcex/.env dotenv_config_debug=true'
+        },
+        {
+            name:"adamant",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/adamant',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/adamant/.env dotenv_config_debug=true'
+        },
+        {
+            name:"art",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/art',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/art/.env dotenv_config_debug=true'
+        },
+        {
+            name:"bats",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/bats',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/bats/.env dotenv_config_debug=true'
+        },
+        {
+            name:"blackshiba",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/blackshib',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/blackshib/.env dotenv_config_debug=true'
+        },
+        {
+            name:"brick",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/brick',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/brick/.env dotenv_config_debug=true'
+        },
+        {
+            name:"king",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/king',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/king/.env dotenv_config_debug=true'
+        },
+        {
+            name:"mema",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/mema',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/mema/.env dotenv_config_debug=true'
+        },
+        {
+            name:"metaflip",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/metaflip',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/metaflip/.env dotenv_config_debug=true'
+        },
+        {
+            name:"mma",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/mma',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/mma/.env dotenv_config_debug=true'
+        },
+        {
+            name:"payflow",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/payflow',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/payflow/.env dotenv_config_debug=true'
+        },
+        {
+            name:"snail",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/snail',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/snail/.env dotenv_config_debug=true'
+        },{
+            name:"tipsy",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/tipsy',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/tipsy/.env dotenv_config_debug=true'
+        },
+        {
+            name:"transplant",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/transplant',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/transplant/.env dotenv_config_debug=true'
+        },
+        {
+            name:"treat",
+            script: "main.js",
+            cwd:'/home/sokuswap/editions/treat',
+            node_args: '-r dotenv/config main.js dotenv_config_path=/home/sokuswap/editions/treat/.env dotenv_config_debug=true'
+        },
+
+    ]
+}
